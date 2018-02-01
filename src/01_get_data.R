0ec5d6a790ec7a53411fd926cf25152d14c437e6
bgulbis <brian.gulbis@gmail.com>
2018-02-01 14:47:51 -0600

    get initial data

--- src/01_get_data.R
+++ src/01_get_data.R
@@ -2,7 +2,13 @@ library(tidyverse)
 library(lubridate)
 library(edwr)
 
-dir_raw <- "data/raw/2017-01"
+tz <- "US/Central"
+data_month <- mdy("1/1/2017", tz = tz)
+month_abbrv <- format(data_month, "%Y-%m")
+
+dir_raw <- paste0("data/raw/", month_abbrv)
+
+dirr::gzip_files(dir_raw)
 
 opiods <- med_lookup(c("narcotic analgesics", "narcotic analgesic combinations")) %>%
     mutate_at("med.name", str_to_lower) %>%
@@ -13,3 +19,66 @@ mbo_opiods <- concat_encounters(opiods$med.name)
 # run MBO query
 #   * Patients - by Medication (Generic) - Administration Date
 #       - Facility (Curr): HH HERMANN;HH Trans Care;HH Rehab;HH Clinics;HC Childrens
+
+pts <- read_data(dir_raw, "patients", FALSE) %>%
+    as.patients() %>%
+    filter(discharge.datetime >= mdy("1/1/2017", tz = tz))
+
+mbo_id <- concat_encounters(pts$millennium.id, 950)
+
+# run MBO query
+#   * Medications - Inpatient - Prompt
+#   * Pain PCA Pump
+
+# run EDW query
+#   * Identifiers - by Millennium Encounter ID
+
+ids <- read_data(dir_raw, "identifiers") %>%
+    as.id()
+
+edw_id <- concat_encounters(ids$pie.id, 950)
+
+# run EDW query
+#   * Service History
+
+services <- read_data(dir_raw, "services") %>%
+    as.services() %>%
+    arrange(pie.id, start.datetime)
+
+demog <- read_data(dir_raw, "demographics", FALSE) %>%
+    as.demographics(extras = list("service.dc" = "Medical Service (Curr)"))
+
+# pca --------------------------------------------------
+
+pca_actions <- c("pca continuous rate dose" = "pca_rate",
+                 "pca demand dose unit" = "pca_dose_unit",
+                 "pca demand dose" = "pca_dose",
+                 "pca doses delivered" = "pca_delivered",
+                 "pca drug" = "pca_drug",
+                 "pca loading dose" = "pca_load",
+                 "pca lockout interval \\(minutes\\)" = "pca_lockout",
+                 "pca total demands" = "pca_demands")
+
+# filter for current month
+pain_pca <- read_data(dir_raw, "pain-pca", FALSE) %>%
+    as.pain_scores() %>%
+    select(millennium.id:event.result) %>%
+    mutate_at("event", str_replace_all, pattern = pca_actions) %>%
+    distinct(millennium.id, event.datetime, event, .keep_all = TRUE) %>%
+    spread(event, event.result) %>%
+    mutate_at(c("pca_demands",
+                "pca_dose",
+                "pca_delivered",
+                "pca_load",
+                "pca_lockout",
+                "pca_rate"),
+              as.numeric) %>%
+    group_by(millennium.id, event.datetime) %>%
+    mutate(total_dose = sum(pca_delivered * pca_dose, pca_load, na.rm = TRUE))
+
+data_pca <- pain_pca %>%
+    group_by(millennium.id, pca_drug) %>%
+    summarize_at(c("pca_demands",
+                   "pca_delivered",
+                   "total_dose"),
+                 sum, na.rm = TRUE)
