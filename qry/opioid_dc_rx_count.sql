3c91069247b86cdf0e66a11c3499430655c2e5d0
Brian Gulbis <brian.gulbis@gmail.com>
2019-07-15 20:00:47 +0000

    qry to count opioids by nurse unit / provider role

---
+++ qry/opioid_dc_rx_count.sql
@@ -0,0 +1,119 @@
+SELECT DISTINCT
+	ORDERS.ORDER_ID AS ORDER_ID,
+	ORDER_CATALOG.PRIMARY_MNEMONIC AS MEDICATION,
+    CV_NURSE_UNIT.DISPLAY AS NURSE_UNIT,
+	CV_POSITION.DISPLAY AS PROVIDER_POSITION
+FROM
+	CODE_VALUE CV_NURSE_UNIT,
+	CODE_VALUE CV_POSITION,
+	ENCNTR_LOC_HIST,
+	ENCOUNTER,
+	ORDER_ACTION,
+	ORDER_CATALOG,
+	ORDERS,
+	PI_THERA_CLASS_VIEW,
+	PRSNL
+WHERE
+	PI_THERA_CLASS_VIEW.DRUG_CAT IN ('narcotic analgesic combinations', 'narcotic analgesics')
+	AND (
+		PI_THERA_CLASS_VIEW.DRUG_CAT_CD = ORDERS.CATALOG_CD
+		AND ORDERS.ACTIVE_IND = 1
+		AND ORDERS.CATALOG_TYPE_CD = 1363
+		AND ORDERS.ORIG_ORD_AS_FLAG = 1
+		AND	ORDERS.TEMPLATE_ORDER_FLAG IN (0, 1)
+	)
+	AND PI_THERA_CLASS_VIEW.DRUG_CAT_CD = ORDER_CATALOG.CATALOG_CD
+	AND (
+		ORDERS.ENCNTR_ID = ENCOUNTER.ENCNTR_ID
+		AND ENCOUNTER.ACTIVE_IND = 1
+		AND ENCOUNTER.LOC_FACILITY_CD IN (3310, 3821, 3822, 3823)
+		AND ENCOUNTER.ENCNTR_TYPE_CD <> 29527
+	)
+	AND (
+		ORDERS.ENCNTR_ID = ENCNTR_LOC_HIST.ENCNTR_ID
+		AND ORDERS.ORIG_ORDER_DT_TM >= ENCNTR_LOC_HIST.TRANSACTION_DT_TM
+		AND (ORDERS.ORIG_ORDER_DT_TM BETWEEN ENCNTR_LOC_HIST.BEG_EFFECTIVE_DT_TM AND ENCNTR_LOC_HIST.END_EFFECTIVE_DT_TM)
+		AND ENCNTR_LOC_HIST.TRANSACTION_DT_TM = (
+			SELECT MAX(ELH.TRANSACTION_DT_TM)
+			FROM ENCNTR_LOC_HIST ELH
+			WHERE
+				ELH.TRANSACTION_DT_TM <= ORDERS.ORIG_ORDER_DT_TM
+				AND ORDERS.ENCNTR_ID = ELH.ENCNTR_ID
+				AND ELH.ACTIVE_IND = 1
+		)
+		AND ENCNTR_LOC_HIST.LOC_NURSE_UNIT_CD = CV_NURSE_UNIT.CODE_VALUE
+	)
+	AND (
+		ORDERS.ORDER_ID = ORDER_ACTION.ORDER_ID
+		AND ORDER_ACTION.ACTION_TYPE_CD = 1376
+		AND ORDER_ACTION.ORDER_PROVIDER_ID = PRSNL.PERSON_ID
+	)
+	AND PRSNL.POSITION_CD = CV_POSITION.CODE_VALUE
+	AND	(
+		ORDERS.ORIG_ORDER_DT_TM + 0
+			BETWEEN DECODE(
+				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
+				'Today', pi_to_gmt(TRUNC(SYSDATE), pi_time_zone(2, @Variable('BOUSER'))),
+				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - 1, pi_time_zone(2, @Variable('BOUSER'))),
+				'Week to Date', pi_to_gmt(TRUNC(SYSDATE, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
+				'Last Week', pi_to_gmt(TRUNC(SYSDATE - 7, 'DAY'), pi_time_zone(2, @Variable('BOUSER'))),
+				'Last Month', pi_to_gmt(TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
+				'Month to Date', pi_to_gmt(TRUNC(SYSDATE-1, 'MONTH'), pi_time_zone(2, @Variable('BOUSER'))),
+				'User-defined', pi_to_gmt(
+					TO_DATE(
+						@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
+						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+					),
+					pi_time_zone(1, @Variable('BOUSER'))),
+				'N Days Prior', pi_to_gmt(SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {'0'}, User:2080), pi_time_zone(2, @Variable('BOUSER')))
+			)
+			AND DECODE(
+				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
+				'Today', pi_to_gmt(TRUNC(SYSDATE) + (86399 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
+				'Yesterday', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
+				'Week to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
+				'Last Week', pi_to_gmt(TRUNC(SYSDATE, 'DAY') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
+				'Last Month', pi_to_gmt(TRUNC(SYSDATE, 'MONTH') - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
+				'Month to Date', pi_to_gmt(TRUNC(SYSDATE) - (1 / 86400), pi_time_zone(2, @Variable('BOUSER'))),
+				'User-defined', pi_to_gmt(
+					TO_DATE(
+						@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
+						pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+					),
+					pi_time_zone(1, @Variable('BOUSER'))),
+				'N Days Prior', pi_to_gmt(SYSDATE, pi_time_zone(2, @Variable('BOUSER')))
+			)
+		AND ORDERS.ORIG_ORDER_DT_TM
+			BETWEEN DECODE(
+				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
+				'Today', TRUNC(SYSDATE),
+				'Yesterday', TRUNC(SYSDATE) - 1,
+				'Week to Date', TRUNC(SYSDATE, 'DAY'),
+				'Last Week', TRUNC(SYSDATE - 7, 'DAY'),
+				'Last Month', TRUNC(ADD_MONTHS(SYSDATE, -1), 'MONTH'),
+				'Month to Date', TRUNC(SYSDATE-1, 'MONTH'),
+				'User-defined', DECODE(
+					@Prompt('Enter begin date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 00:00:00'}, User:80),
+					'01/01/1800 00:00:00',
+					'',
+					@Variable('Enter begin date (Leave as 01/01/1800 if using a Relative Date)')
+				),
+				'N Days Prior', SYSDATE - @Prompt('Days Prior to Now', 'N', , mono, free, persistent, {0}, User:2080)
+			) - 1
+			AND DECODE(
+				@Prompt('Choose date range', 'A', {'Today', 'Yesterday', 'Week to Date', 'Last Week', 'Last Month', 'Month to Date', 'User-defined', 'N Days Prior'}, mono, free, , , User:79),
+				'Today', TRUNC(SYSDATE) + (86399 / 86400),
+				'Yesterday', TRUNC(SYSDATE) - (1 / 86400),
+				'Week to Date', TRUNC(SYSDATE) - (1 / 86400),
+				'Last Week', TRUNC(SYSDATE, 'DAY') - (1 / 86400),
+				'Last Month', TRUNC(SYSDATE, 'MONTH') - (1 / 86400),
+				'Month to Date', TRUNC(SYSDATE) - (1 / 86400),
+				'User-defined', DECODE(
+					@Prompt('Enter end date (Leave as 01/01/1800 if using a Relative Date)', 'D', , mono, free, persistent, {'01/01/1800 23:59:59'}, User:81),
+					'01/01/1800 00:00:00',
+					'',
+					@Variable('Enter end date (Leave as 01/01/1800 if using a Relative Date)')
+				),
+				'N Days Prior', SYSDATE
+			) + 1
+	)
