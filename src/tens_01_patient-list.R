3ecb9a1ba39ac85d6fdf0a26768a4db68a245a16
Brian Gulbis <brian.gulbis@gmail.com>
2019-08-07 15:47:05 +0000

    get data sets

--- src/tens_01_patient-list.R
+++ src/tens_01_patient-list.R
@@ -1,6 +1,16 @@
 library(tidyverse)
 library(readxl)
+library(mbohelpr)
 
 pt_list <- read_excel("data/external/tens_patient-list_baseline.xlsx")
 
 fin_mbo <- edwr::concat_encounters(pt_list$`Enc - Patient Account`)
+print(fin_mbo)
+
+# data sets -----------------------------------------
+data_dir <- "data/tidy/tens"
+
+data_pt <- get_data(data_dir, "pt_form_data")
+data_meds <- get_data(data_dir, "pain_meds")
+data_surg <- get_data(data_dir, "surgeries")
+data_pain <- get_data(data_dir, "pain_scores")
