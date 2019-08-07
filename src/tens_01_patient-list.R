64e229bd1eab733c0355463b1bfefee88deb06a8
Brian Gulbis <brian.gulbis@gmail.com>
2019-08-07 16:11:05 +0000

    surgery date/time; calc post-op day

--- src/tens_01_patient-list.R
+++ src/tens_01_patient-list.R
@@ -14,3 +14,28 @@ data_pt <- get_data(data_dir, "pt_form_data")
 data_meds <- get_data(data_dir, "pain_meds")
 data_surg <- get_data(data_dir, "surgeries")
 data_pain <- get_data(data_dir, "pain_scores")
+
+# tidy data -----------------------------------------
+
+df_surg_start <- data_surg %>% 
+    arrange(fin, surgery_start_datetime, desc(primary_procedure)) %>%
+    distinct(fin, surg_case_id, .keep_all = TRUE)
+
+df_surg_first <- df_surg_start %>%
+    distinct(fin, .keep_all = TRUE) %>%
+    select(fin, surgery_start_datetime, surgery_stop_datetime)
+
+df_meds <- data_meds %>%
+    inner_join(df_surg_first, by = "fin") %>%
+    arrange(fin, event_datetime) %>%
+    filter(event_datetime >= surgery_stop_datetime) %>%
+    mutate(
+        surg_med_days = difftime(
+            event_datetime,
+            surgery_stop_datetime,
+            units = "days"
+        ),
+        postop_day = floor(surg_med_days)
+    )
+
+# calculate morphine equivalents
