9bfb89c368d5ade91f32b11b7611fe602e614e16
Brian Gulbis <brian.gulbis@gmail.com>
2021-03-03 13:55:57 -0600

    before / after comparison

--- qry/apap_dsu_orders.sql
+++ qry/apap_dsu_orders.sql
@@ -12,8 +12,20 @@ WITH DSU_PTS AS (
 	WHERE
 		ENCOUNTER.ORGANIZATION_ID = 1 -- Memorial Hermann Hospital
 		AND ENCOUNTER.REG_DT_TM BETWEEN
-			pi_to_gmt(TRUNC(ADD_MONTHS(SYSDATE, -6), 'MONTH'), 'America/Chicago')
-			AND pi_to_gmt(TRUNC(SYSDATE, 'MONTH') - 1/86400, 'America/Chicago')
+			pi_to_gmt(
+				TO_DATE(
+					@Prompt('Enter begin date', 'D', , mono, free, persistent, {'07/01/2020 00:00:00'}, User:0), 
+					pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+				), 
+				'America/Chicago'
+			)
+			AND pi_to_gmt(
+				TO_DATE(
+					@Prompt('Enter end date', 'D', , mono, free, persistent, {'3/01/2021 00:00:00'}, User:1), 
+					pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+				) - 1/86400, 
+				'America/Chicago'
+			)
 		AND ENCOUNTER.ENCNTR_ID = ENCNTR_LOC_HIST.ENCNTR_ID
 		AND ENCNTR_LOC_HIST.LOC_NURSE_UNIT_CD = 43683728 -- HH DSU
 ), APAP_ORDERS AS(
@@ -98,8 +110,20 @@ WITH DSU_PTS AS (
 		AND DSU_PTS.PERSON_ID = CLINICAL_EVENT.PERSON_ID
 		AND CLINICAL_EVENT.EVENT_CD = 37556009 -- acetaminophen
 		AND CLINICAL_EVENT.EVENT_END_DT_TM BETWEEN
-			pi_to_gmt(TRUNC(ADD_MONTHS(SYSDATE, -6), 'MONTH'), 'America/Chicago')
-			AND pi_to_gmt(TRUNC(SYSDATE, 'MONTH') - 1/86400, 'America/Chicago')
+			pi_to_gmt(
+				TO_DATE(
+					@Prompt('Enter begin date', 'D', , mono, free, persistent, {'07/01/2020 00:00:00'}, User:0), 
+					pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+				), 
+				'America/Chicago'
+			)
+			AND pi_to_gmt(
+				TO_DATE(
+					@Prompt('Enter end date', 'D', , mono, free, persistent, {'3/01/2021 00:00:00'}, User:1), 
+					pi_get_dm_info_char_gen('Date Format Mask|FT','PI EXP|Systems Configuration|Date Format Mask')
+				) - 1/86400, 
+				'America/Chicago'
+			)
 		AND CLINICAL_EVENT.VALID_UNTIL_DT_TM > DATE '2099-12-31'
 		AND CLINICAL_EVENT.EVENT_ID = CE_MED_RESULT.EVENT_ID
 		AND CE_MED_RESULT.VALID_UNTIL_DT_TM > DATE '2099-12-31'
